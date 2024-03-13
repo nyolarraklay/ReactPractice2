c2a01cfebfbe33bb0a42da9d56da33b0b00f120f
nyolarraklay <114483893+nyolarraklay@users.noreply.github.com>
2024-03-13 14:19:02 +0100

    cartIcon value apperars only if with value

--- ReactPractice2/src/Components/CartIcon/index.jsx
+++ ReactPractice2/src/Components/CartIcon/index.jsx
@@ -7,7 +7,7 @@ const CartIcon = ({ onClick }) => {
     return (
       <div className="cart-icon" onClick={onClick}>
            <div className="shopping-cart-icon"> <IoCartOutline /> 
-                <span className="item-count">{ getTotalNumbersOfItemsInCart()} </span>
+           {getTotalNumbersOfItemsInCart() > 0 && <span className="item-count">{ getTotalNumbersOfItemsInCart()} </span>}
             </div>
     {/* //avoid passing ffunctions, because once you click on the button it will create a lot of ffunctions */}
       </div>
