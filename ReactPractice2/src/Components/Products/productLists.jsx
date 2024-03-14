05d2c14dfd007dc1e7bfbf1966eee0e1951dbe19
nyolarraklay <114483893+nyolarraklay@users.noreply.github.com>
2024-03-14 21:26:04 +0100

    fix errors and console

--- ReactPractice2/src/Components/Products/productLists.jsx
+++ ReactPractice2/src/Components/Products/productLists.jsx
@@ -2,6 +2,11 @@ import styled from 'styled-components'
 import useProductStore from '../Store'
 import { Link } from 'react-router-dom'
 
+const Paragraph = styled.p`
+  color: ${(props)=>(props.isDiscounted > 0 ? 'red' : 'white')};
+  text-decoration: ${(props)=>(props.isDiscounted > 0 ? 'line-through' : 'none')};
+`;
+
 
 
 function Product({ product: { title, description, price, id, discountedPrice }, onAddToCart, image}) {
@@ -10,17 +15,11 @@ const { seeMore, getAddedToCartValue} = useProductStore();
 const addedToCart = getAddedToCartValue(id);
 const discount = Math.floor((price - discountedPrice) / price * 100);
 
-const Paragraph = styled.p`
-  color: ${discount > 0 ? 'red' : 'white'};
-  text-decoration: ${discount > 0 ? 'line-through' : 'none'};
-`;
-
 
   function handleButtonClick() {
     onAddToCart(id);   
   }
 
-console.log(discount);
   return <div className="card">
     <img src={image} alt={title} className="productImage" />
     <div className="cardContent">
@@ -30,7 +29,7 @@ console.log(discount);
   
     <h3>TITLE: {title}</h3>
     <p>Description: {description}</p>
-    <Paragraph>Price: {price} kr</Paragraph>
+    <Paragraph isDiscounted={discount}>Price: {price} kr</Paragraph>
     {discount > 0 && <div><p>NOW! {discountedPrice}</p> <p>On Sale: {discount}% Off</p></div>}
     
     <button onClick={handleButtonClick}>Add to cart</button>
